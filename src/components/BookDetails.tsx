1d4c2e152a1621bc141dea369c0a722a3920cf7b
Ville Leppänen <vileppanen@gmail.com>
2023-03-21 20:42:12 +0200

    handle corner cases

--- src/components/BookDetails.tsx
+++ src/components/BookDetails.tsx
@@ -7,14 +7,16 @@ interface BookDetailsProps {
 }
 export const BookDetails = (props: BookDetailsProps): JSX.Element => {
   const { book } = props
-  const reviews = book.reviews.map(review => (<span className='book-review'><a href={review.url}>Review by {review.byLine}</a></span>))
+  const reviews = (book.reviews)
+    ? book.reviews.map(review => (<span className='book-review'><a href={review.url}>Review by {review.byLine}</a></span>))
+    : [<span className='book-review'>Unable to retrieve reviews</span>]
   return (
     <div className='book-details'>
       <div className='book-detail-entry'>
           <span>{book.rank}. {book.title} - {book.author}</span>
        </div>
       <div className='book-detail-entry'>
-          <span>{book.isbn10}</span>
+          <span>{book.isbn10 ?? book.isbn13}</span>
        </div>
        <RenderGate renderIf={reviews.length > 0}>
         {reviews}
